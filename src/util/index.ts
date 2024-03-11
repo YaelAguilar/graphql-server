4ba991d7e476379263bd3374ffda263e222f171c
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-03-11 07:45:33 -0600

    [ADD] interfaces to models

--- src/util/index.ts
+++
@@ -1,5 +0,0 @@
-import { encryptPassword, comparePassword } from '../infrastructure/bcrypt/bcrypt';
-import { createJWTToken } from '../infrastructure/auth/auth';
-
-
-export { encryptPassword, comparePassword, createJWTToken };
